5892d4fd67eb144bab31e78e087c6c40798dba61
Erick Tryzelaar <erick.tryzelaar@gmail.com>
2014-08-14 19:03:51 -0700

    implement to_json for most tuples

--- src/json/mod.rs
+++ src/json/mod.rs
@@ -2218,7 +2218,7 @@ macro_rules! impl_to_json_tuple {
     }
 }
 
-impl_to_json_tuple! { T0, T1, } // T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, }
+impl_to_json_tuple! { T0, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, }
 
 impl<A:ToJson> ToJson for Vec<A> {
     fn to_json(&self) -> Json { List(self.iter().map(|elt| elt.to_json()).collect()) }
