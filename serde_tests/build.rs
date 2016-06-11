021f4f2d70c6af4ab5eefb813723ded8cbbc1c7c
David Tolnay <dtolnay@gmail.com>
2016-06-10 22:55:27 -0700

    Use serde_codegen::expand in serde_tests/build.rs

--- serde_tests/build.rs
+++ serde_tests/build.rs
@@ -13,10 +13,6 @@ fn main() {
     ] {
         let src = Path::new(src);
         let dst = Path::new(&out_dir).join(dst);
-
-        let mut registry = syntex::Registry::new();
-
-        serde_codegen::register(&mut registry);
-        registry.expand("", &src, &dst).unwrap();
+        serde_codegen::expand(&src, &dst).unwrap();
     }
 }
