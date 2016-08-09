8bd7acc9fc4ec3f9f80f5a8bd52393547fe48908
Oliver Schneider <git-spam-no-reply9815368754983@oli-obk.de>
2016-08-09 13:08:16 +0200

    use a thread in the build script of serde_codegen to allow env vars to control the stack size

--- serde_codegen/build.rs
+++ serde_codegen/build.rs
@@ -4,14 +4,17 @@ mod inner {
 
     use std::env;
     use std::path::Path;
+    use std::thread::spawn;
 
     pub fn main() {
-        let out_dir = env::var_os("OUT_DIR").unwrap();
+        // put everything into a thread, so users can use `RUST_MIN_STACK` to increase the amount of stack
+        spawn(|| {
+            let out_dir = env::var_os("OUT_DIR").unwrap();
 
-        let src = Path::new("src/lib.rs.in");
-        let dst = Path::new(&out_dir).join("lib.rs");
-
-        quasi_codegen::expand(&src, &dst).unwrap();
+            let src = Path::new("src/lib.rs.in");
+            let dst = Path::new(&out_dir).join("lib.rs");
+            quasi_codegen::expand(&src, &dst).unwrap();
+        }).join().unwrap()
     }
 }
 
