defcbef7ab133c7bca996ef01430f0acd99e64cb
Jon Gjengset <jon@thesquareplanet.com>
2017-03-03 23:28:35 -0500

    Use a non-stupid path for bytes::ByteBuf

--- serde/src/de/impls.rs
+++ serde/src/de/impls.rs
@@ -55,7 +55,7 @@ use de::{Deserialize, Deserializer, EnumVisitor, Error, MapVisitor, SeqVisitor, 
          VariantVisitor, Visitor};
 use de::from_primitive::FromPrimitive;
 
-use super::super::bytes::ByteBuf;
+use bytes::ByteBuf;
 
 ///////////////////////////////////////////////////////////////////////////////
 
