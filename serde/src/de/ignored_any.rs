7d0a38270df8f89c651a7c598cad929fa367d80f
David Tolnay <dtolnay@gmail.com>
2017-04-13 11:06:48 -0700

    Eagerly implement common traits for IgnoredAny

--- serde/src/de/ignored_any.rs
+++ serde/src/de/ignored_any.rs
@@ -4,7 +4,7 @@ use de::{Deserialize, Deserializer, Visitor, SeqVisitor, MapVisitor, Error};
 
 /// A target for deserializers that want to ignore data. Implements Deserialize
 /// and silently eats data given to it.
-#[derive(Debug)]
+#[derive(Copy, Clone, Debug, Default)]
 pub struct IgnoredAny;
 
 impl<'de> Deserialize<'de> for IgnoredAny {
