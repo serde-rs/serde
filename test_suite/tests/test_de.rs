3f920f645c4ba614775fff9e6d914ecd3a8184f7
David Tolnay <dtolnay@gmail.com>
2017-04-13 17:34:42 -0700

    License boilerplate

--- test_suite/tests/test_de.rs
+++ test_suite/tests/test_de.rs
@@ -1,3 +1,11 @@
+// Copyright 2017 Serde Developers
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
 #![cfg_attr(feature = "unstable", feature(into_boxed_c_str))]
 
 #[macro_use]
