alexcrichton/rust-serde

Update to the proc_macro2 crate and rewritten `syn`

serde_derive/src/de.rs
@@ -6,8 +6,10 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
-use syn::{self, Ident};
+use proc_macro2::Term;
 use quote::{self, Tokens, ToTokens};
+use syn::{self, Ident};
+use synom::delimited::Delimited;
 
 use bound;
 use fragment::{Fragment, Expr, Stmts, Match};
@@ -15,6 +17,7 @@ use internals::ast::{Body, Container, Field, Style, Variant};
 use internals::{self, attr};
 
 use std::collections::BTreeSet;
+use std::mem;
 
 pub fn expand_derive_deserialize(input: &syn::DeriveInput) -> Result<Tokens, String> {
     let ctxt = internals::Ctxt::new();
@@ -24,7 +27,7 @@ pub fn expand_derive_deserialize(input: &syn::DeriveInput) -> Result<Tokens, Str
     let ident = &cont.ident;
     let params = Parameters::new(&cont);
     let (de_impl_generics, _, ty_generics, where_clause) = split_with_de_lifetime(&params);
-    let dummy_const = Ident::new(format!("_IMPL_DESERIALIZE_FOR_{}", ident));
+    let dummy_const = Ident::from(format!("_IMPL_DESERIALIZE_FOR_{}", ident));
     let body = Stmts(deserialize_body(&cont, &params));
 
     let impl_block = if let Some(remote) = cont.attrs.remote() {
@@ -104,14 +107,15 @@ impl Parameters {
     /// Type name to use in error messages and `&'static str` arguments to
     /// various Deserializer methods.
     fn type_name(&self) -> &str {
-        self.this.segments.last().unwrap().ident.as_ref()
+        self.this.segments.get(self.this.segments.len() - 1).item().ident.as_ref()
     }
 
     fn de_lifetime_def(&self) -> syn::LifetimeDef {
         syn::LifetimeDef {
             attrs: Vec::new(),
-            lifetime: syn::Lifetime::new("'de"),
-            bounds: self.borrowed.iter().cloned().collect(),
+            lifetime: syn::Lifetime::new(Term::intern("'de"), Default::default()),
+            bounds: self.borrowed.iter().cloned().collect::<Vec<_>>().into(),
+            colon_token: Default::default(),
         }
     }
 }
@@ -163,7 +167,10 @@ fn needs_deserialize_bound(attrs: &attr::Field) -> bool {
 // Fields with a `default` attribute (not `default=...`), and fields with a
 // `skip_deserializing` attribute that do not also have `default=...`.
 fn requires_default(attrs: &attr::Field) -> bool {
-    attrs.default() == &attr::Default::Default
+    match *attrs.default() {
+        attr::Default::Default => true,
+        _ => false,
+    }
 }
 
 // The union of lifetimes borrowed by each field of the container.
@@ -1654,7 +1661,7 @@ fn deserialize_map(
 }
 
 fn field_i(i: usize) -> Ident {
-    Ident::new(format!("__field{}", i))
+    Ident::from(format!("__field{}", i))
 }
 
 /// This function wraps the expression in `#[serde(deserialize_with = "...")]`
@@ -1732,7 +1739,15 @@ struct DeImplGenerics<'a>(&'a Parameters);
 impl<'a> ToTokens for DeImplGenerics<'a> {
     fn to_tokens(&self, tokens: &mut Tokens) {
         let mut generics = self.0.generics.clone();
-        generics.lifetimes.insert(0, self.0.de_lifetime_def());
+        let mut lifetimes = Delimited::new();
+        lifetimes.push_first(self.0.de_lifetime_def());
+        for lifetime in mem::replace(&mut generics.lifetimes, Default::default()) {
+            if !lifetimes.trailing_delim() {
+                lifetimes.push_trailing(Default::default());
+            }
+            lifetimes.push(lifetime);
+        }
+        generics.lifetimes = lifetimes;
         let (impl_generics, _, _) = generics.split_for_impl();
         impl_generics.to_tokens(tokens);
     }
@@ -1743,9 +1758,16 @@ struct DeTyGenerics<'a>(&'a syn::Generics);
 impl<'a> ToTokens for DeTyGenerics<'a> {
     fn to_tokens(&self, tokens: &mut Tokens) {
         let mut generics = self.0.clone();
-        generics
-            .lifetimes
-            .insert(0, syn::LifetimeDef::new("'de"));
+        let lifetime = syn::Lifetime::new(Term::intern("'de"), Default::default());
+        let mut lifetimes = Delimited::new();
+        lifetimes.push_first(syn::LifetimeDef::new(lifetime));
+        for lifetime in mem::replace(&mut generics.lifetimes, Default::default()) {
+            if !lifetimes.trailing_delim() {
+                lifetimes.push_trailing(Default::default());
+            }
+            lifetimes.push(lifetime);
+        }
+        generics.lifetimes = lifetimes;
         let (_, ty_generics, _) = generics.split_for_impl();
         ty_generics.to_tokens(tokens);
     }

serde_derive_internals/src/ast.rs
@@ -7,6 +7,7 @@
 // except according to those terms.
 
 use syn;
+use synom::delimited::Delimited;
 use attr;
 use check;
 use Ctxt;
@@ -49,9 +50,9 @@ impl<'a> Container<'a> {
         let attrs = attr::Container::from_ast(cx, item);
 
         let mut body = match item.body {
-            syn::Body::Enum(ref variants) => Body::Enum(enum_from_ast(cx, variants)),
-            syn::Body::Struct(ref variant_data) => {
-                let (style, fields) = struct_from_ast(cx, variant_data);
+            syn::Body::Enum(ref variants) => Body::Enum(enum_from_ast(cx, &variants.variants)),
+            syn::Body::Struct(ref data) => {
+                let (style, fields) = struct_from_ast(cx, &data.data);
                 Body::Struct(style, fields)
             }
         };
@@ -98,11 +99,14 @@ impl<'a> Body<'a> {
     }
 }
 
-fn enum_from_ast<'a>(cx: &Ctxt, variants: &'a [syn::Variant]) -> Vec<Variant<'a>> {
+fn enum_from_ast<'a, T>(cx: &Ctxt, variants: &'a Delimited<syn::Variant, T>)
+    -> Vec<Variant<'a>>
+{
     variants
         .iter()
         .map(
             |variant| {
+                let variant = variant.item();
                 let (style, fields) = struct_from_ast(cx, &variant.data);
                 Variant {
                     ident: variant.ident.clone(),
@@ -117,25 +121,27 @@ fn enum_from_ast<'a>(cx: &Ctxt, variants: &'a [syn::Variant]) -> Vec<Variant<'a>
 
 fn struct_from_ast<'a>(cx: &Ctxt, data: &'a syn::VariantData) -> (Style, Vec<Field<'a>>) {
     match *data {
-        syn::VariantData::Struct(ref fields) => (Style::Struct, fields_from_ast(cx, fields)),
-        syn::VariantData::Tuple(ref fields) if fields.len() == 1 => {
+        syn::VariantData::Struct(ref fields, _) => (Style::Struct, fields_from_ast(cx, fields)),
+        syn::VariantData::Tuple(ref fields, _) if fields.len() == 1 => {
             (Style::Newtype, fields_from_ast(cx, fields))
         }
-        syn::VariantData::Tuple(ref fields) => (Style::Tuple, fields_from_ast(cx, fields)),
+        syn::VariantData::Tuple(ref fields, _) => (Style::Tuple, fields_from_ast(cx, fields)),
         syn::VariantData::Unit => (Style::Unit, Vec::new()),
     }
 }
 
-fn fields_from_ast<'a>(cx: &Ctxt, fields: &'a [syn::Field]) -> Vec<Field<'a>> {
+fn fields_from_ast<'a, T>(cx: &Ctxt, fields: &'a Delimited<syn::Field, T>)
+    -> Vec<Field<'a>>
+{
     fields
         .iter()
         .enumerate()
         .map(
             |(i, field)| {
                 Field {
-                    ident: field.ident.clone(),
-                    attrs: attr::Field::from_ast(cx, i, field),
-                    ty: &field.ty,
+                    ident: field.item().ident.clone(),
+                    attrs: attr::Field::from_ast(cx, i, field.item()),
+                    ty: &field.item().ty,
                 }
             },
         )

serde-rs/serde

serde_derive/src/de.rs
@@ -6,8 +6,12 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
-use syn::{self, Ident};
+use proc_macro2::Term;
 use quote::{self, Tokens, ToTokens};
+use syn::{self, Ident};
+use syn::delimited::Delimited;
+use syn::tokens::{Comma, Colon, Lt, Gt};
+use syn::Span;
 
 use bound;
 use fragment::{Fragment, Expr, Stmts, Match};
@@ -15,6 +19,7 @@ use internals::ast::{Body, Container, Field, Style, Variant};
 use internals::{self, attr};
 
 use std::collections::BTreeSet;
+use std::mem;
 
 pub fn expand_derive_deserialize(input: &syn::DeriveInput) -> Result<Tokens, String> {
     let ctxt = internals::Ctxt::new();
@@ -24,7 +29,7 @@ pub fn expand_derive_deserialize(input: &syn::DeriveInput) -> Result<Tokens, Str
     let ident = &cont.ident;
     let params = Parameters::new(&cont);
     let (de_impl_generics, _, ty_generics, where_clause) = split_with_de_lifetime(&params);
-    let dummy_const = Ident::new(format!("_IMPL_DESERIALIZE_FOR_{}", ident));
+    let dummy_const = Ident::from(format!("_IMPL_DESERIALIZE_FOR_{}", ident));
     let body = Stmts(deserialize_body(&cont, &params));
 
     let impl_block = if let Some(remote) = cont.attrs.remote() {
@@ -104,14 +109,19 @@ impl Parameters {
     /// Type name to use in error messages and `&'static str` arguments to
     /// various Deserializer methods.
     fn type_name(&self) -> &str {
-        self.this.segments.last().unwrap().ident.as_ref()
+        self.this.segments.last().unwrap().item().ident.as_ref()
     }
 
     fn de_lifetime_def(&self) -> syn::LifetimeDef {
         syn::LifetimeDef {
             attrs: Vec::new(),
-            lifetime: syn::Lifetime::new("'de"),
+            lifetime: syn::Lifetime::new(Term::intern("'de"), Span::default()),
             bounds: self.borrowed.iter().cloned().collect(),
+            colon_token: if self.borrowed.len() > 0 {
+                Some(Colon::default())
+            } else {
+                None
+            },
         }
     }
 }
@@ -163,7 +173,7 @@ fn needs_deserialize_bound(attrs: &attr::Field) -> bool {
 // Fields with a `default` attribute (not `default=...`), and fields with a
 // `skip_deserializing` attribute that do not also have `default=...`.
 fn requires_default(attrs: &attr::Field) -> bool {
-    attrs.default() == &attr::Default::Default
+    *attrs.default() == attr::Default::Default
 }
 
 // The union of lifetimes borrowed by each field of the container.
@@ -1654,7 +1664,7 @@ fn deserialize_map(
 }
 
 fn field_i(i: usize) -> Ident {
-    Ident::new(format!("__field{}", i))
+    Ident::from(format!("__field{}", i))
 }
 
 /// This function wraps the expression in `#[serde(deserialize_with = "...")]`
@@ -1732,7 +1742,22 @@ struct DeImplGenerics<'a>(&'a Parameters);
 impl<'a> ToTokens for DeImplGenerics<'a> {
     fn to_tokens(&self, tokens: &mut Tokens) {
         let mut generics = self.0.generics.clone();
-        generics.lifetimes.insert(0, self.0.de_lifetime_def());
+        let mut lifetimes = Delimited::new();
+        lifetimes.push_first(self.0.de_lifetime_def());
+        for lifetime in mem::replace(&mut generics.lifetimes, Delimited::default()) {
+            if !lifetimes.trailing_delim() {
+                lifetimes.push_trailing(Comma::default());
+            }
+            lifetimes.push(lifetime);
+        }
+        if generics.ty_params.len() > 0 && !lifetimes.trailing_delim() {
+            lifetimes.push_trailing(Comma::default());
+        }
+        if generics.lt_token.is_none() {
+            generics.lt_token = Some(Lt::default());
+            generics.gt_token = Some(Gt::default());
+        }
+        generics.lifetimes = lifetimes;
         let (impl_generics, _, _) = generics.split_for_impl();
         impl_generics.to_tokens(tokens);
     }
@@ -1743,9 +1768,23 @@ struct DeTyGenerics<'a>(&'a syn::Generics);
 impl<'a> ToTokens for DeTyGenerics<'a> {
     fn to_tokens(&self, tokens: &mut Tokens) {
         let mut generics = self.0.clone();
-        generics
-            .lifetimes
-            .insert(0, syn::LifetimeDef::new("'de"));
+        let lifetime = syn::Lifetime::new(Term::intern("'de"), Span::default());
+        let mut lifetimes = Delimited::new();
+        lifetimes.push_first(syn::LifetimeDef::new(lifetime));
+        for lifetime in mem::replace(&mut generics.lifetimes, Delimited::default()) {
+            if !lifetimes.trailing_delim() {
+                lifetimes.push_trailing(Comma::default());
+            }
+            lifetimes.push(lifetime);
+        }
+        if generics.ty_params.len() > 0 && !lifetimes.trailing_delim() {
+            lifetimes.push_trailing(Comma::default());
+        }
+        if generics.lt_token.is_none() {
+            generics.lt_token = Some(Lt::default());
+            generics.gt_token = Some(Gt::default());
+        }
+        generics.lifetimes = lifetimes;
         let (_, ty_generics, _) = generics.split_for_impl();
         ty_generics.to_tokens(tokens);
     }

serde_derive_internals/src/ast.rs
@@ -7,6 +7,7 @@
 // except according to those terms.
 
 use syn;
+use syn::delimited::Delimited;
 use attr;
 use check;
 use Ctxt;
@@ -49,9 +50,9 @@ impl<'a> Container<'a> {
         let attrs = attr::Container::from_ast(cx, item);
 
         let mut body = match item.body {
-            syn::Body::Enum(ref variants) => Body::Enum(enum_from_ast(cx, variants)),
-            syn::Body::Struct(ref variant_data) => {
-                let (style, fields) = struct_from_ast(cx, variant_data);
+            syn::Body::Enum(ref variants) => Body::Enum(enum_from_ast(cx, &variants.variants)),
+            syn::Body::Struct(ref data) => {
+                let (style, fields) = struct_from_ast(cx, &data.data);
                 Body::Struct(style, fields)
             }
         };
@@ -98,11 +99,14 @@ impl<'a> Body<'a> {
     }
 }
 
-fn enum_from_ast<'a>(cx: &Ctxt, variants: &'a [syn::Variant]) -> Vec<Variant<'a>> {
+fn enum_from_ast<'a, T>(cx: &Ctxt, variants: &'a Delimited<syn::Variant, T>)
+    -> Vec<Variant<'a>>
+{
     variants
         .iter()
         .map(
             |variant| {
+                let variant = variant.item();
                 let (style, fields) = struct_from_ast(cx, &variant.data);
                 Variant {
                     ident: variant.ident.clone(),
@@ -117,25 +121,27 @@ fn enum_from_ast<'a>(cx: &Ctxt, variants: &'a [syn::Variant]) -> Vec<Variant<'a>
 
 fn struct_from_ast<'a>(cx: &Ctxt, data: &'a syn::VariantData) -> (Style, Vec<Field<'a>>) {
     match *data {
-        syn::VariantData::Struct(ref fields) => (Style::Struct, fields_from_ast(cx, fields)),
-        syn::VariantData::Tuple(ref fields) if fields.len() == 1 => {
+        syn::VariantData::Struct(ref fields, _) => (Style::Struct, fields_from_ast(cx, fields)),
+        syn::VariantData::Tuple(ref fields, _) if fields.len() == 1 => {
             (Style::Newtype, fields_from_ast(cx, fields))
         }
-        syn::VariantData::Tuple(ref fields) => (Style::Tuple, fields_from_ast(cx, fields)),
+        syn::VariantData::Tuple(ref fields, _) => (Style::Tuple, fields_from_ast(cx, fields)),
         syn::VariantData::Unit => (Style::Unit, Vec::new()),
     }
 }
 
-fn fields_from_ast<'a>(cx: &Ctxt, fields: &'a [syn::Field]) -> Vec<Field<'a>> {
+fn fields_from_ast<'a, T>(cx: &Ctxt, fields: &'a Delimited<syn::Field, T>)
+    -> Vec<Field<'a>>
+{
     fields
         .iter()
         .enumerate()
         .map(
             |(i, field)| {
                 Field {
-                    ident: field.ident.clone(),
-                    attrs: attr::Field::from_ast(cx, i, field),
-                    ty: &field.ty,
+                    ident: field.item().ident.clone(),
+                    attrs: attr::Field::from_ast(cx, i, field.item()),
+                    ty: &field.item().ty,
                 }
             },
         )
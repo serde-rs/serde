370d4ef1061db03f8ca3424eef38433f57e6e536
Alex Crichton <alex@alexcrichton.com>
2017-07-06 14:03:21 -0700

    Update to the proc_macro2 crate and rewritten `syn`

--- test_suite/tests/compile-fail/remote/bad_getter.rs
+++ test_suite/tests/compile-fail/remote/bad_getter.rs
@@ -18,7 +18,7 @@ mod remote {
 #[derive(Serialize)] //~ ERROR: proc-macro derive panicked
 #[serde(remote = "remote::S")]
 struct S {
-    #[serde(getter = "~~~")] //~^^^ HELP: failed to parse path: "~~~"
+    #[serde(getter = "~~~")] //~^^^ HELP: failed to parse path
     a: u8,
 }
 
