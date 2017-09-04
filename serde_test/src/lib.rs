d4042872f5afa48a25fea933bfb749388f76dd8d
David Tolnay <dtolnay@gmail.com>
2017-09-04 11:11:44 -0700

    Release 1.0.12

--- serde_test/src/lib.rs
+++ serde_test/src/lib.rs
@@ -155,7 +155,7 @@
 //! # }
 //! ```
 
-#![doc(html_root_url = "https://docs.rs/serde_test/1.0.11")]
+#![doc(html_root_url = "https://docs.rs/serde_test/1.0.12")]
 
 #[macro_use]
 extern crate serde;
