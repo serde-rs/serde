8a630fea7c55a37598dda98687fb24d5366b1439
David Tolnay <dtolnay@gmail.com>
2017-09-09 11:08:19 -0700

    Suppress cast_lossless lint in test suite

--- test_suite/tests/test_annotations.rs
+++ test_suite/tests/test_annotations.rs
@@ -6,6 +6,8 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
+#![cfg_attr(feature = "cargo-clippy", allow(cast_lossless))]
+
 #[macro_use]
 extern crate serde_derive;
 
