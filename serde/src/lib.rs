1d74f45837391f9a7e279a0737f4831560e48b7a
Josh Chase <josh@jec.pw>
2017-11-29 14:51:04 -0500

    Port ser::Error and de::Error to use failure

--- serde/src/lib.rs
+++ serde/src/lib.rs
@@ -137,6 +137,8 @@ extern crate alloc;
 #[cfg(all(feature = "unstable", feature = "std"))]
 extern crate core;
 
+extern crate failure;
+
 /// A facade around all the types we need from the `std`, `core`, and `alloc`
 /// crates. This avoids elaborate import wrangling having to happen in every
 /// module.
