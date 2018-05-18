9f114548f4a34e4e001f5521213b6b8499a4d71d
David Tolnay <dtolnay@gmail.com>
2018-05-18 11:48:05 -0700

    Revert "Use `version_check` crate instead of handcrafted version parsing"
    This reverts commit 8890061f8213d605b9dff36d9e950f7d081b74b5.

--- serde/build.rs
+++ serde/build.rs
@@ -1,13 +1,39 @@
-extern crate version_check;
+use std::env;
+use std::process::Command;
+use std::str::{self, FromStr};
 
 fn main() {
-    match version_check::is_min_version("1.26.0") {
-        Some((true, _)) => {
-            println!("cargo:rustc-cfg=integer128");
-        },
-        Some((false, _)) => {}
-        None => {
-            println!("could not figure out the rustc version");
-        },
+    let rustc = match env::var_os("RUSTC") {
+        Some(rustc) => rustc,
+        None => return,
     };
+
+    let output = match Command::new(rustc).arg("--version").output() {
+        Ok(output) => output,
+        Err(_) => return,
+    };
+
+    let version = match str::from_utf8(&output.stdout) {
+        Ok(version) => version,
+        Err(_) => return,
+    };
+
+    let mut pieces = version.split('.');
+    if pieces.next() != Some("rustc 1") {
+        return;
+    }
+
+    let next = match pieces.next() {
+        Some(next) => next,
+        None => return,
+    };
+
+    let minor = match u32::from_str(next) {
+        Ok(minor) => minor,
+        Err(_) => return,
+    };
+
+    if minor >= 26 {
+        println!("cargo:rustc-cfg=integer128");
+    }
 }
