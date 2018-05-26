7ec3cac7d6ed13aeaa80820258ecd5c27decbe41
David Tolnay <dtolnay@gmail.com>
2018-05-26 09:48:50 -0700

    Stabilize Deserialize for Box<CStr>

--- serde/src/de/impls.rs
+++ serde/src/de/impls.rs
@@ -573,7 +573,7 @@ macro_rules! forwarded_impl {
     }
 }
 
-#[cfg(all(feature = "std", feature = "unstable"))]
+#[cfg(all(feature = "std", de_boxed_c_str))]
 forwarded_impl!((), Box<CStr>, CString::into_boxed_c_str);
 
 ////////////////////////////////////////////////////////////////////////////////
