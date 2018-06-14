f779c8903960c81dff4ac05122a0eb00b5c108c0
Niklas Adofsson <niklas@parity.io>
2018-06-14 18:42:02 +0200

    Use `cfg guards` if `cargo-test` used for no-std

--- test_suite/no_std/src/main.rs
+++ test_suite/no_std/src/main.rs
@@ -16,10 +16,12 @@ fn start(_argc: isize, _argv: *const *const u8) -> isize {
     0
 }
 
+#[cfg(not(test))]
 #[lang = "eh_personality"]
 #[no_mangle]
 pub extern "C" fn rust_eh_personality() {}
 
+#[cfg(not(test))]
 #[panic_implementation]
 fn panic(_info: &core::panic::PanicInfo) -> ! {
     unsafe {
