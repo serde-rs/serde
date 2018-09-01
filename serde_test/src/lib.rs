55cecace2904bb3b24c2bbe122a176806dafc395
David Tolnay <dtolnay@gmail.com>
2018-09-01 15:25:53 -0700

    Release 1.0.76

--- serde_test/src/lib.rs
+++ serde_test/src/lib.rs
@@ -161,7 +161,7 @@
 //! # }
 //! ```
 
-#![doc(html_root_url = "https://docs.rs/serde_test/1.0.75")]
+#![doc(html_root_url = "https://docs.rs/serde_test/1.0.76")]
 #![cfg_attr(feature = "cargo-clippy", deny(clippy, clippy_pedantic))]
 // Whitelisted clippy lints
 #![cfg_attr(feature = "cargo-clippy", allow(float_cmp))]
