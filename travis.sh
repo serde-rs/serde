2cf10a600370b987a35136d21216d5dc29f7ecba
David Tolnay <dtolnay@gmail.com>
2018-09-15 15:25:16 -0700

    Remove separate proc-macro2/nightly build
    This feature is automatically enabled on sufficiently new compilers.

--- travis.sh
+++ travis.sh
@@ -68,7 +68,6 @@ else
     channel build
     cd "$DIR/test_suite"
     channel test --features unstable
-    channel build --tests --features proc-macro2/nightly
     if [ -z "${APPVEYOR}" ]; then
         cd "$DIR/test_suite/no_std"
         channel build
