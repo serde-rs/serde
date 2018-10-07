778e51627052ffd6b505512d48be03095ccd0eea
David Tolnay <dtolnay@gmail.com>
2018-10-06 21:12:14 -0700

    Feature panic_handler has been stabilized

--- test_suite/no_std/src/main.rs
+++ test_suite/no_std/src/main.rs
@@ -6,7 +6,7 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
-#![feature(lang_items, start, panic_handler)]
+#![feature(lang_items, start)]
 #![no_std]
 
 extern crate libc;
