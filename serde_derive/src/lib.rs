1b45e5766a30db85010112ea6d2d2638eddd9b12
David Tolnay <dtolnay@gmail.com>
2018-10-14 03:08:53 -0700

    Release 1.0.80

--- serde_derive/src/lib.rs
+++ serde_derive/src/lib.rs
@@ -22,7 +22,7 @@
 //!
 //! [https://serde.rs/derive.html]: https://serde.rs/derive.html
 
-#![doc(html_root_url = "https://docs.rs/serde_derive/1.0.79")]
+#![doc(html_root_url = "https://docs.rs/serde_derive/1.0.80")]
 #![cfg_attr(feature = "cargo-clippy", allow(renamed_and_removed_lints))]
 #![cfg_attr(feature = "cargo-clippy", deny(clippy, clippy_pedantic))]
 // Whitelisted clippy lints
--- serde_test/src/lib.rs
+++ serde_test/src/lib.rs
@@ -161,7 +161,7 @@
 //! # }
 //! ```
 
-#![doc(html_root_url = "https://docs.rs/serde_test/1.0.79")]
+#![doc(html_root_url = "https://docs.rs/serde_test/1.0.80")]
 #![cfg_attr(feature = "cargo-clippy", allow(renamed_and_removed_lints))]
 #![cfg_attr(feature = "cargo-clippy", deny(clippy, clippy_pedantic))]
 // Whitelisted clippy lints
