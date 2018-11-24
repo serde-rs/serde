b3d9d51b517cce50547edd7454709fb6f311fe25
David Tolnay <dtolnay@gmail.com>
2018-11-24 15:35:49 -0800

    Simplify compiletest setup

--- test_suite/tests/compiletest.rs
+++ test_suite/tests/compiletest.rs
@@ -10,22 +10,14 @@
 
 extern crate compiletest_rs as compiletest;
 
-use std::env;
-
-fn run_mode(mode: &'static str) {
-    let mut config = compiletest::Config::default();
-
-    config.mode = mode.parse().expect("invalid mode");
-    config.target_rustcflags = Some("-L deps/target/debug/deps".to_owned());
-    if let Ok(name) = env::var("TESTNAME") {
-        config.filter = Some(name);
-    }
-    config.src_base = format!("tests/{}", mode).into();
-
-    compiletest::run_tests(&config);
-}
-
 #[test]
 fn compile_fail() {
-    run_mode("compile-fail");
+    let config = compiletest::Config {
+        mode: compiletest::common::Mode::CompileFail,
+        src_base: std::path::PathBuf::from("tests/compile-fail"),
+        target_rustcflags: Some("-L deps/target/debug/deps".to_owned()),
+        ..Default::default()
+    };
+
+    compiletest::run_tests(&config);
 }
