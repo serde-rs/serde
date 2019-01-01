c95ee3968a14ee9d7ee4eec903c3c6f931e8fbf3
David Tolnay <dtolnay@gmail.com>
2018-12-31 22:04:56 -0500

    Format with rustfmt 2018-12-10

--- test_suite/tests/compiletest.rs
+++ test_suite/tests/compiletest.rs
@@ -7,12 +7,14 @@ fn ui() {
     let config = compiletest::Config {
         mode: compiletest::common::Mode::Ui,
         src_base: std::path::PathBuf::from("tests/ui"),
-        target_rustcflags: Some(String::from("\
-            --edition=2018 \
-            -L deps/target/debug/deps \
-            -Z unstable-options \
-            --extern serde_derive \
-        ")),
+        target_rustcflags: Some(String::from(
+            "\
+             --edition=2018 \
+             -L deps/target/debug/deps \
+             -Z unstable-options \
+             --extern serde_derive \
+             ",
+        )),
         ..Default::default()
     };
 
