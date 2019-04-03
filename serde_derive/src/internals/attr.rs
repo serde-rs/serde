f3c6b9f05a8df75f050680cc077368d723f8854c
David Tolnay <dtolnay@gmail.com>
2019-04-03 09:18:45 -0700

    Simplify signature of Container::serde_path

--- serde_derive/src/internals/attr.rs
+++ serde_derive/src/internals/attr.rs
@@ -687,7 +687,7 @@ impl Container {
         self.serde_path.as_ref()
     }
 
-    pub fn serde_path<'a>(&'a self) -> Cow<'a, syn::Path> {
+    pub fn serde_path(&self) -> Cow<syn::Path> {
         self.custom_serde_path()
             .map(Cow::Borrowed)
             .unwrap_or_else(|| Cow::Owned(parse_quote!(_serde)))
