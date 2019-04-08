e9cd73f78e4d8dc03189ef11afa28df3f4f7000b
David Tolnay <dtolnay@gmail.com>
2019-04-08 10:39:51 -0700

    Remove link to hjson
    This project has still not been updated to Serde 1.0.

--- serde/src/lib.rs
+++ serde/src/lib.rs
@@ -46,7 +46,6 @@
 //! - [BSON], the data storage and network transfer format used by MongoDB.
 //! - [Avro], a binary format used within Apache Hadoop, with support for schema
 //!   definition.
-//! - [Hjson], a variant of JSON designed to be readable and writable by humans.
 //! - [JSON5], A superset of JSON including some productions from ES5.
 //! - [URL], the x-www-form-urlencoded format.
 //! - [Envy], a way to deserialize environment variables into Rust structs.
@@ -64,7 +63,6 @@
 //! [RON]: https://github.com/ron-rs/ron
 //! [BSON]: https://github.com/zonyitoo/bson-rs
 //! [Avro]: https://github.com/flavray/avro-rs
-//! [Hjson]: https://github.com/laktak/hjson-rust
 //! [JSON5]: https://github.com/callum-oakley/json5-rs
 //! [URL]: https://github.com/nox/serde_urlencoded
 //! [Envy]: https://github.com/softprops/envy
