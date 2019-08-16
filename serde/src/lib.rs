d6e5947ad219ebb5fc1518bd99605bb38bb51da3
David Tolnay <dtolnay@gmail.com>
2019-08-16 11:24:22 -0700

    Suppress warnings about try! macro

--- serde/src/lib.rs
+++ serde/src/lib.rs
@@ -81,7 +81,7 @@
 //
 //    https://github.com/serde-rs/serde/issues/812
 #![cfg_attr(feature = "unstable", feature(specialization, never_type))]
-#![allow(unknown_lints, bare_trait_objects)]
+#![allow(unknown_lints, bare_trait_objects, deprecated)]
 #![cfg_attr(feature = "cargo-clippy", allow(renamed_and_removed_lints))]
 #![cfg_attr(feature = "cargo-clippy", deny(clippy, clippy_pedantic))]
 // Ignored clippy and clippy_pedantic lints
