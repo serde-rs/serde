32fc779a8702622aaa8efdc18377878ba521e12b
Mingun <Alexander_Sergey@mail.ru>
2020-11-03 22:27:36 +0500

    Allow to deserialize newtype structs from SeqAccessDeserializer

--- serde_core/src/de/value.rs
+++ serde_core/src/de/value.rs
@@ -1227,9 +1227,16 @@ where
         self.deserialize_unit(visitor)
     }
 
+    fn deserialize_newtype_struct<V>(self, _name: &str, visitor: V) -> Result<V::Value, Self::Error>
+    where
+        V: de::Visitor<'de>,
+    {
+        visitor.visit_newtype_struct(self)
+    }
+
     forward_to_deserialize_any! {
         bool i8 i16 i32 i64 i128 u8 u16 u32 u64 u128 f32 f64 char str string
-        bytes byte_buf option newtype_struct seq tuple
+        bytes byte_buf option seq tuple
         tuple_struct map struct enum identifier ignored_any
     }
 }
