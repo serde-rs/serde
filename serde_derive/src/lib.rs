e7974312681c9c1c54f7586e86b0d7013b350a54
David Tolnay <dtolnay@gmail.com>
2020-12-05 13:45:47 -0800

    Release 1.0.118

--- serde_derive/src/lib.rs
+++ serde_derive/src/lib.rs
@@ -13,7 +13,7 @@
 //!
 //! [https://serde.rs/derive.html]: https://serde.rs/derive.html
 
-#![doc(html_root_url = "https://docs.rs/serde_derive/1.0.117")]
+#![doc(html_root_url = "https://docs.rs/serde_derive/1.0.118")]
 #![allow(unknown_lints, bare_trait_objects)]
 #![deny(clippy::all, clippy::pedantic)]
 // Ignored clippy lints
