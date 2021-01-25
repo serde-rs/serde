d1975f3661a09053fe069e00cc45176f96033183
David Tolnay <dtolnay@gmail.com>
2021-01-24 16:07:38 -0800

    Update serde_derive_internals to tool attrs

--- serde_derive_internals/lib.rs
+++ serde_derive_internals/lib.rs
@@ -1,18 +1,15 @@
 #![doc(html_root_url = "https://docs.rs/serde_derive_internals/0.25.0")]
 #![allow(unknown_lints, bare_trait_objects)]
-#![cfg_attr(feature = "cargo-clippy", allow(renamed_and_removed_lints))]
-#![cfg_attr(
-    feature = "cargo-clippy",
-    allow(
-        cognitive_complexity,
-        redundant_field_names,
-        result_unit_err,
-        should_implement_trait,
-        trivially_copy_pass_by_ref,
-        wildcard_in_or_patterns,
-        // clippy bug: https://github.com/rust-lang/rust-clippy/issues/5704
-        unnested_or_patterns,
-    )
+#![deny(clippy::all)]
+#![allow(
+    clippy::cognitive_complexity,
+    clippy::redundant_field_names,
+    clippy::result_unit_err,
+    clippy::should_implement_trait,
+    clippy::trivially_copy_pass_by_ref,
+    clippy::wildcard_in_or_patterns,
+    // clippy bug: https://github.com/rust-lang/rust-clippy/issues/5704
+    clippy::unnested_or_patterns,
 )]
 
 #[macro_use]
