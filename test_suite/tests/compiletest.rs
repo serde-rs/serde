f6c104fd1da0a330a425c0344c1cf027f68f2b20
David Tolnay <dtolnay@gmail.com>
2022-04-24 16:00:18 -0700

    Ignore unused ignore attribute warning in some configurations

--- test_suite/tests/compiletest.rs
+++ test_suite/tests/compiletest.rs
@@ -1,6 +1,7 @@
 #[cfg_attr(target_os = "emscripten", ignore)]
 #[rustversion::attr(not(nightly), ignore)]
 #[cfg_attr(miri, ignore)]
+#[allow(unused_attributes)]
 #[test]
 fn ui() {
     let t = trybuild::TestCases::new();
