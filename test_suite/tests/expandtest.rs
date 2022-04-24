9f29f6bb4ad5ec7db6b1b1ad787f668bd74c104e
David Tolnay <dtolnay@gmail.com>
2022-04-24 16:01:52 -0700

    Disable expandtest on miri

--- test_suite/tests/expandtest.rs
+++ test_suite/tests/expandtest.rs
@@ -1,6 +1,7 @@
 #[cfg_attr(target_os = "emscripten", ignore)]
 #[cfg_attr(not(expandtest), ignore)]
 #[rustversion::attr(not(nightly), ignore)]
+#[cfg_attr(miri, ignore)]
 #[allow(unused_attributes)]
 #[test]
 fn expandtest() {
