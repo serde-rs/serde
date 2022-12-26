ccf9c6fc072378ea8c4f15df1024e258d35d6e61
David Tolnay <dtolnay@gmail.com>
2022-12-26 09:21:13 -0800

    Release 1.0.152

--- serde_test/src/lib.rs
+++ serde_test/src/lib.rs
@@ -140,7 +140,7 @@
 //! # }
 //! ```
 
-#![doc(html_root_url = "https://docs.rs/serde_test/1.0.151")]
+#![doc(html_root_url = "https://docs.rs/serde_test/1.0.152")]
 #![cfg_attr(feature = "cargo-clippy", allow(renamed_and_removed_lints))]
 // Ignored clippy lints
 #![cfg_attr(feature = "cargo-clippy", allow(float_cmp, needless_doctest_main))]
