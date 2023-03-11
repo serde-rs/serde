2ba406726f9f84bc3b65ce4e824ae636dfa7dc85
David Tolnay <dtolnay@gmail.com>
2023-03-11 12:57:53 -0800

    Release 1.0.155

--- serde_derive/src/lib.rs
+++ serde_derive/src/lib.rs
@@ -13,7 +13,7 @@
 //!
 //! [https://serde.rs/derive.html]: https://serde.rs/derive.html
 
-#![doc(html_root_url = "https://docs.rs/serde_derive/1.0.154")]
+#![doc(html_root_url = "https://docs.rs/serde_derive/1.0.155")]
 #![allow(unknown_lints, bare_trait_objects)]
 // Ignored clippy lints
 #![allow(
