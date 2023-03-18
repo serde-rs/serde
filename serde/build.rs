c42e7c8012bc906fecb3c030221dc5bef51761a6
David Tolnay <dtolnay@gmail.com>
2023-03-17 17:34:46 -0700

    Reflect serde_derive required compiler in build script and rust-version metadata

--- serde/build.rs
+++ serde/build.rs
@@ -78,11 +78,6 @@ fn main() {
         println!("cargo:rustc-cfg=no_num_nonzero");
     }
 
-    // Current minimum supported version of serde_derive crate is Rust 1.31.
-    if minor < 31 {
-        println!("cargo:rustc-cfg=no_serde_derive");
-    }
-
     // TryFrom, Atomic types, non-zero signed integers, and SystemTime::checked_add
     // stabilized in Rust 1.34:
     // https://blog.rust-lang.org/2019/04/11/Rust-1.34.0.html#tryfrom-and-tryinto
@@ -92,6 +87,11 @@ fn main() {
         println!("cargo:rustc-cfg=no_num_nonzero_signed");
         println!("cargo:rustc-cfg=no_systemtime_checked_add");
         println!("cargo:rustc-cfg=no_relaxed_trait_bounds");
+    }
+
+    // Current minimum supported version of serde_derive crate is Rust 1.56.
+    if minor < 56 {
+        println!("cargo:rustc-cfg=no_serde_derive");
     }
 
     // Support for #[cfg(target_has_atomic = "...")] stabilized in Rust 1.60.
