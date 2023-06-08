a3982379302ea6111a2ac219ad5aa78c57ba1a6a
David Tolnay <dtolnay@gmail.com>
2023-06-07 21:48:16 -0700

    Point out serde(untagged) variants which are out of order
    Previously if someone wrote an enum containing:
    
    - `A` (untagged)
    - `B` (tagged)
    - `C` (tagged)
    - `D` (untagged)
    - `E` (tagged)
    - `F` (untagged)
    
    serde_derive would produce errors referring to B and E only, saying
    you're supposed to put untagged variants at the end. The choice of B and
    E for this error doesn't make a lot of sense because in order to resolve
    the issue, the user must either:
    
    - move A and D down
    
    or:
    
    - move B, C, and E up.
    
    This commit changes the error to appear on A and D instead.

--- serde_derive/src/internals/ast.rs
+++ serde_derive/src/internals/ast.rs
@@ -140,8 +140,7 @@ fn enum_from_ast<'a>(
     variants: &'a Punctuated<syn::Variant, Token![,]>,
     container_default: &attr::Default,
 ) -> Vec<Variant<'a>> {
-    let mut seen_untagged = false;
-    variants
+    let variants: Vec<Variant> = variants
         .iter()
         .map(|variant| {
             let attrs = attr::Variant::from_ast(cx, variant);
@@ -154,12 +153,21 @@ fn enum_from_ast<'a>(
                 fields,
                 original: variant,
             }
-        }).inspect(|variant| {
-            if !variant.attrs.untagged() && seen_untagged {
+        })
+        .collect();
+
+    let index_of_last_tagged_variant = variants
+        .iter()
+        .rposition(|variant| !variant.attrs.untagged());
+    if let Some(index_of_last_tagged_variant) = index_of_last_tagged_variant {
+        for variant in &variants[..index_of_last_tagged_variant] {
+            if variant.attrs.untagged() {
                 cx.error_spanned_by(&variant.ident, "all variants with the #[serde(untagged)] attribute must be placed at the end of the enum");
             }
-            seen_untagged = variant.attrs.untagged();
-        }).collect()
+        }
+    }
+
+    variants
 }
 
 fn struct_from_ast<'a>(
