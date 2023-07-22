50e2f4b21370e40b6c3735952a6983954c84172d
David Tolnay <dtolnay@gmail.com>
2023-07-22 08:26:18 -0700

    Leak all memory allocated during macro expansion

--- precompiled/bin/main.rs
+++ precompiled/bin/main.rs
@@ -2,8 +2,26 @@ extern crate proc_macro2;
 
 use proc_macro2::watt;
 use proc_macro2::watt::buffer::InputBuffer;
+use std::alloc::{GlobalAlloc, Layout, System};
 use std::io::{self, Read, Write};
 use std::sync::atomic::Ordering;
+
+struct MonotonicAllocator;
+
+#[global_allocator]
+static ALLOCATOR: MonotonicAllocator = MonotonicAllocator;
+
+unsafe impl GlobalAlloc for MonotonicAllocator {
+    unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
+        System.alloc(layout)
+    }
+
+    unsafe fn dealloc(&self, _ptr: *mut u8, _layout: Layout) {
+        // Leak: this cuts 3% of code size from the precompiled macro binary.
+        // There is no way that serde_derive would fill up all memory on the
+        // host. When the subprocess exits, operating system will clean this up.
+    }
+}
 
 fn main() {
     let mut buf = Vec::new();
