291ec50d98dc4bb0b309d13b9cbdcd5a2baf4765
Mingun <alexander_sergey@mail.ru>
2023-07-22 22:33:09 +0500

    Add tests that ensures that error reported for a path for `with` and `default` attributes

---
+++ test_suite/tests/ui/default-attribute/incorrect_type_enum_externally_tagged.rs
@@ -0,0 +1,21 @@
+//! Ensures that error message points to the path in attribute
+use serde_derive::Deserialize;
+
+#[derive(Deserialize)]
+enum Enum {
+    // Newtype variants does not use the provided path, so it is forbidden here
+    // Newtype(#[serde(default = "main")] u8),
+    Tuple(
+        u8,
+        #[serde(default = "main")] i8,
+    ),
+    Struct {
+        #[serde(default = "main")]
+        f1: u8,
+        f2: u8,
+        #[serde(default = "main")]
+        f3: i8,
+    },
+}
+
+fn main() {}
---
+++ test_suite/tests/ui/default-attribute/incorrect_type_tuple.rs
@@ -0,0 +1,11 @@
+//! Ensures that error message points to the path in attribute
+use serde_derive::Deserialize;
+
+#[derive(Deserialize)]
+#[serde(default = "main")]
+struct Tuple(
+    u8,
+    #[serde(default = "main")] i8,
+);
+
+fn main() {}
