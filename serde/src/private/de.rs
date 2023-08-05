c2b0bc77b0f135553ee072d7f0981fbec67cb253
Mingun <Alexander_Sergey@mail.ru>
2023-08-05 21:28:46 +0500

    Implement deserialize_float using deserialize_integer
    thus we can be sure that all integers also would be handled when floats are requested

--- serde/src/private/de.rs
+++ serde/src/private/de.rs
@@ -334,15 +334,7 @@ mod content {
             match *self {
                 Content::F32(v) => visitor.visit_f32(v),
                 Content::F64(v) => visitor.visit_f64(v),
-                Content::U8(v) => visitor.visit_u8(v),
-                Content::U16(v) => visitor.visit_u16(v),
-                Content::U32(v) => visitor.visit_u32(v),
-                Content::U64(v) => visitor.visit_u64(v),
-                Content::I8(v) => visitor.visit_i8(v),
-                Content::I16(v) => visitor.visit_i16(v),
-                Content::I32(v) => visitor.visit_i32(v),
-                Content::I64(v) => visitor.visit_i64(v),
-                _ => Err(self.invalid_type(&visitor)),
+                _ => self.deserialize_integer(visitor),
             }
         }
     }
