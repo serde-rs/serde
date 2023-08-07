eafa12ad09adb5577a3df066f4bec0dd1c930e14
Mingun <alexander_sergey@mail.ru>
2023-08-07 21:20:28 +0500

    Rename externally tagged enum Inner to Enum

--- test_suite/tests/test_enum_internally_tagged.rs
+++ test_suite/tests/test_enum_internally_tagged.rs
@@ -525,11 +525,11 @@ fn newtype_variant_containing_externally_tagged_enum() {
     #[derive(Debug, PartialEq, Serialize, Deserialize)]
     #[serde(tag = "tag")]
     enum Outer {
-        Inner(Inner),
+        Inner(Enum),
     }
 
     #[derive(Debug, PartialEq, Serialize, Deserialize)]
-    enum Inner {
+    enum Enum {
         Unit,
         Newtype(u8),
         Tuple(u8, u8),
@@ -537,7 +537,7 @@ fn newtype_variant_containing_externally_tagged_enum() {
     }
 
     assert_tokens(
-        &Outer::Inner(Inner::Unit),
+        &Outer::Inner(Enum::Unit),
         &[
             Token::Map { len: Some(2) },
             Token::Str("tag"),
@@ -549,7 +549,7 @@ fn newtype_variant_containing_externally_tagged_enum() {
     );
 
     assert_tokens(
-        &Outer::Inner(Inner::Newtype(1)),
+        &Outer::Inner(Enum::Newtype(1)),
         &[
             Token::Map { len: Some(2) },
             Token::Str("tag"),
@@ -564,7 +564,7 @@ fn newtype_variant_containing_externally_tagged_enum() {
     // Content::Seq case
     // via ContentDeserializer::deserialize_enum
     assert_tokens(
-        &Outer::Inner(Inner::Tuple(1, 1)),
+        &Outer::Inner(Enum::Tuple(1, 1)),
         &[
             Token::Map { len: Some(2) },
             Token::Str("tag"),
@@ -585,7 +585,7 @@ fn newtype_variant_containing_externally_tagged_enum() {
     // Content::Map case
     // via ContentDeserializer::deserialize_enum
     assert_tokens(
-        &Outer::Inner(Inner::Struct { f: 1 }),
+        &Outer::Inner(Enum::Struct { f: 1 }),
         &[
             Token::Map { len: Some(2) },
             Token::Str("tag"),
@@ -606,7 +606,7 @@ fn newtype_variant_containing_externally_tagged_enum() {
     // Content::Seq case
     // via ContentDeserializer::deserialize_enum
     assert_de_tokens(
-        &Outer::Inner(Inner::Struct { f: 1 }),
+        &Outer::Inner(Enum::Struct { f: 1 }),
         &[
             Token::Map { len: Some(2) },
             Token::Str("tag"),
