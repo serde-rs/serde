b1ca2473cec8d5c419e3dd89b61e5d6a94539df7
Mingun <alexander_sergey@mail.ru>
2023-08-07 20:11:39 +0500

    Allow to deserialize unit and unit structs from SeqAccessDeserializer
    failures (1):
        unit_variant_with_unknown_fields
    
    Fixed (1):
        newtype_unit_struct

--- serde_core/src/de/value.rs
+++ serde_core/src/de/value.rs
@@ -1203,9 +1203,31 @@ where
         visitor.visit_seq(self.seq)
     }
 
+    fn deserialize_unit<V>(self, visitor: V) -> Result<V::Value, Self::Error>
+    where
+        V: de::Visitor<'de>,
+    {
+        // Covered by tests/test_enum_internally_tagged.rs
+        //      newtype_unit
+        visitor.visit_unit()
+    }
+
+    fn deserialize_unit_struct<V>(
+        self,
+        _name: &'static str,
+        visitor: V,
+    ) -> Result<V::Value, Self::Error>
+    where
+        V: de::Visitor<'de>,
+    {
+        // Covered by tests/test_enum_internally_tagged.rs
+        //      newtype_unit_struct
+        self.deserialize_unit(visitor)
+    }
+
     forward_to_deserialize_any! {
         bool i8 i16 i32 i64 i128 u8 u16 u32 u64 u128 f32 f64 char str string
-        bytes byte_buf option unit unit_struct newtype_struct seq tuple
+        bytes byte_buf option newtype_struct seq tuple
         tuple_struct map struct enum identifier ignored_any
     }
 }
