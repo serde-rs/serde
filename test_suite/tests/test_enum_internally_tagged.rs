c4e9f7d99b2bcd571a1d3421ae77696f0022cac6
Mingun <alexander_sergey@mail.ru>
2023-08-07 21:36:16 +0500

    Share unit struct Unit between all tests

--- test_suite/tests/test_enum_internally_tagged.rs
+++ test_suite/tests/test_enum_internally_tagged.rs
@@ -13,6 +13,9 @@ use serde_test::{assert_de_tokens, assert_de_tokens_error, assert_tokens, Token}
 use std::collections::BTreeMap;
 
 #[derive(Debug, PartialEq, Serialize, Deserialize)]
+struct Unit;
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
 struct Struct {
     f: u8,
 }
@@ -617,9 +620,6 @@ fn newtype_variant_containing_externally_tagged_enum() {
 
 #[test]
 fn newtype_variant_containing_unit_struct() {
-    #[derive(Debug, PartialEq, Serialize, Deserialize)]
-    struct Unit;
-
     #[derive(Debug, PartialEq, Serialize, Deserialize)]
     #[serde(tag = "tag")]
     enum Message {
