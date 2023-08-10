9951eca32487cc2197ad597041a11d337de15095
Mingun <alexander_sergey@mail.ru>
2023-08-10 19:03:39 +0500

    Inline #match_keys

--- serde_derive/src/de/struct_.rs
+++ serde_derive/src/de/struct_.rs
@@ -294,17 +294,6 @@ fn deserialize_map(
         })
     };
 
-    let match_keys = quote! {
-        let mut __seed = #field_seed;
-        while let _serde::#private::Some(__key) = _serde::de::MapAccess::next_key_seed(&mut __map, &mut __seed)? {
-            match __key {
-                #(#value_arms)*
-                #field_enum::Field(_) => unreachable!(),
-                #ignored_arm
-            }
-        }
-    };
-
     let extract_values = fields_names
         .iter()
         .filter(|&&(field, _)| !field.attrs.skip_deserializing() && !field.attrs.flatten())
@@ -399,7 +388,14 @@ fn deserialize_map(
 
         #let_collect
 
-        #match_keys
+        let mut __seed = #field_seed;
+        while let _serde::#private::Some(__key) = _serde::de::MapAccess::next_key_seed(&mut __map, &mut __seed)? {
+            match __key {
+                #(#value_arms)*
+                #field_enum::Field(_) => unreachable!(),
+                #ignored_arm
+            }
+        }
 
         #let_default
 
@@ -585,17 +581,6 @@ fn deserialize_map_in_place(
         })
     };
 
-    let match_keys = quote! {
-        let mut __seed = #field_seed;
-        while let _serde::#private::Some(__key) = _serde::de::MapAccess::next_key_seed(&mut __map, &mut __seed)? {
-            match __key {
-                #(#value_arms_from)*
-                #field_enum::Field(_) => unreachable!(),
-                #ignored_arm
-            }
-        }
-    };
-
     let check_flags = fields_names
         .iter()
         .filter(|&&(field, _)| !field.attrs.skip_deserializing())
@@ -648,7 +633,14 @@ fn deserialize_map_in_place(
     quote_block! {
         #(#let_flags)*
 
-        #match_keys
+        let mut __seed = #field_seed;
+        while let _serde::#private::Some(__key) = _serde::de::MapAccess::next_key_seed(&mut __map, &mut __seed)? {
+            match __key {
+                #(#value_arms_from)*
+                #field_enum::Field(_) => unreachable!(),
+                #ignored_arm
+            }
+        }
 
         #let_default
 
