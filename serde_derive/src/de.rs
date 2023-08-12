8cd44cfc6be75de3c0180b304175c6c590d9263f
Mingun <alexander_sergey@mail.ru>
2023-08-12 15:33:04 +0500

    Use deserialize_map when deserialize internally tagged enums
    Deserializer methods are only hints which deserializer is not obliged to follow.
    
    Both
    - TaggedContentVisitor
    - InternallyTaggedUnitVisitor
    
    accepts only visit_map and visit_seq and that is what derived implementation of
    Deserialize does for structs. Therefore it is fine to call deserialize_map here,
    as that already did in derived deserialize implementation. Because those structs
    officially not public, it is used only by derive macro

--- serde_derive/src/de.rs
+++ serde_derive/src/de.rs
@@ -1055,7 +1055,7 @@ fn deserialize_struct(
             _serde::de::VariantAccess::struct_variant(__variant, FIELDS, #visitor_expr)
         },
         StructForm::InternallyTagged(_, deserializer) => quote! {
-            _serde::Deserializer::deserialize_any(#deserializer, #visitor_expr)
+            _serde::Deserializer::deserialize_map(#deserializer, #visitor_expr)
         },
         StructForm::Untagged(_, deserializer) => quote! {
             _serde::Deserializer::deserialize_any(#deserializer, #visitor_expr)
@@ -1436,7 +1436,7 @@ fn deserialize_internally_tagged_enum(
             }
         }
 
-        _serde::Deserializer::deserialize_any(
+        _serde::Deserializer::deserialize_map(
             __deserializer,
             _serde::__private::de::TaggedContentVisitor::<__Seed>::new(#tag, #expecting)
         )
@@ -1892,7 +1892,7 @@ fn deserialize_internally_tagged_variant(
                 quote!((#default))
             });
             quote_block! {
-                _serde::Deserializer::deserialize_any(#deserializer, _serde::__private::de::InternallyTaggedUnitVisitor::new(#type_name, #variant_name))?;
+                _serde::Deserializer::deserialize_map(#deserializer, _serde::__private::de::InternallyTaggedUnitVisitor::new(#type_name, #variant_name))?;
                 _serde::__private::Ok(#this_value::#variant_ident #default)
             }
         }
