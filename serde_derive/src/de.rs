e93a650e9bd1ef98b2b428f10487ae92aa7665cc
Mingun <alexander_sergey@mail.ru>
2023-08-12 15:33:04 +0500

    Add support for internally tagged enums in non self-describing formats
    Deserializer methods are only hints which deserializer is not obliged to follow.
    Both TaggedContentVisitor and InternallyTaggedUnitVisitor accepts only
    visit_map and visit_seq and that is what derived implementation of Deserialize
    does for structs. Therefore it is fine to call deserialize_map here, as that
    already did in derived deserialize implementation

--- serde_derive/src/de.rs
+++ serde_derive/src/de.rs
@@ -1073,7 +1073,7 @@ fn deserialize_struct(
             _serde::de::VariantAccess::struct_variant(__variant, FIELDS, #visitor_expr)
         },
         StructForm::InternallyTagged(_, deserializer) => quote! {
-            _serde::Deserializer::deserialize_any(#deserializer, #visitor_expr)
+            _serde::Deserializer::deserialize_map(#deserializer, #visitor_expr)
         },
         StructForm::Untagged(_, deserializer) => quote! {
             _serde::Deserializer::deserialize_any(#deserializer, #visitor_expr)
@@ -1411,7 +1411,7 @@ fn deserialize_internally_tagged_enum(
 
         #variants_stmt
 
-        let (__tag, __content) = _serde::Deserializer::deserialize_any(
+        let (__tag, __content) = _serde::Deserializer::deserialize_map(
             __deserializer,
             _serde::__private::de::TaggedContentVisitor::<__Field>::new(#tag, #expecting))?;
         let __deserializer = _serde::__private::de::ContentDeserializer::<__D::Error>::new(__content);
@@ -1876,7 +1876,7 @@ fn deserialize_internally_tagged_variant(
                 quote!((#default))
             });
             quote_block! {
-                _serde::Deserializer::deserialize_any(#deserializer, _serde::__private::de::InternallyTaggedUnitVisitor::new(#type_name, #variant_name))?;
+                _serde::Deserializer::deserialize_map(#deserializer, _serde::__private::de::InternallyTaggedUnitVisitor::new(#type_name, #variant_name))?;
                 _serde::__private::Ok(#this_value::#variant_ident #default)
             }
         }
