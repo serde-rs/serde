146dc0fad527e73813154334960c9c41f00f5cac
David Tolnay <dtolnay@gmail.com>
2023-08-25 10:53:36 -0700

    Release 1.0.187

--- serde_derive/src/lib.rs
+++ serde_derive/src/lib.rs
@@ -13,7 +13,7 @@
 //!
 //! [https://serde.rs/derive.html]: https://serde.rs/derive.html
 
-#![doc(html_root_url = "https://docs.rs/serde_derive/1.0.186")]
+#![doc(html_root_url = "https://docs.rs/serde_derive/1.0.187")]
 // Ignored clippy lints
 #![allow(
     // clippy false positive: https://github.com/rust-lang/rust-clippy/issues/7054
