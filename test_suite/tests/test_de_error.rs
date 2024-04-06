5b24f88e73caa9c607527b5b4696fc34263cd238
David Tolnay <dtolnay@gmail.com>
2024-04-05 19:18:03 -0700

    Resolve legacy_numeric_constants clippy lints
        warning: usage of a legacy numeric method
           --> serde_derive/src/ser.rs:292:51
            |
        292 |     assert!(fields.len() as u64 <= u64::from(u32::max_value()));
            |                                                   ^^^^^^^^^^^
            |
            = help: for further information visit https://rust-lang.github.io/rust-clippy/master/index.html#legacy_numeric_constants
            = note: `#[warn(clippy::legacy_numeric_constants)]` on by default
        help: use the associated constant instead
            |
        292 |     assert!(fields.len() as u64 <= u64::from(u32::MAX));
            |                                                   ~~~
    
        warning: usage of a legacy numeric method
           --> serde_derive/src/ser.rs:400:53
            |
        400 |     assert!(variants.len() as u64 <= u64::from(u32::max_value()));
            |                                                     ^^^^^^^^^^^
            |
            = help: for further information visit https://rust-lang.github.io/rust-clippy/master/index.html#legacy_numeric_constants
        help: use the associated constant instead
            |
        400 |     assert!(variants.len() as u64 <= u64::from(u32::MAX));
            |                                                     ~~~
    
        warning: usage of a legacy numeric method
            --> test_suite/tests/test_de_error.rs:1462:29
             |
        1462 |             Token::U64(u64::max_value()),
             |                             ^^^^^^^^^^^
             |
             = help: for further information visit https://rust-lang.github.io/rust-clippy/master/index.html#legacy_numeric_constants
             = note: `-W clippy::legacy-numeric-constants` implied by `-W clippy::all`
             = help: to override `-W clippy::all` add `#[allow(clippy::legacy_numeric_constants)]`
        help: use the associated constant instead
             |
        1462 |             Token::U64(u64::MAX),
             |                             ~~~
    
        warning: usage of a legacy numeric method
            --> test_suite/tests/test_de_error.rs:1479:29
             |
        1479 |             Token::U64(u64::max_value()),
             |                             ^^^^^^^^^^^
             |
             = help: for further information visit https://rust-lang.github.io/rust-clippy/master/index.html#legacy_numeric_constants
        help: use the associated constant instead
             |
        1479 |             Token::U64(u64::MAX),
             |                             ~~~
    
        warning: usage of a legacy numeric method
            --> test_suite/tests/test_de_error.rs:1493:29
             |
        1493 |             Token::U64(u64::max_value()),
             |                             ^^^^^^^^^^^
             |
             = help: for further information visit https://rust-lang.github.io/rust-clippy/master/index.html#legacy_numeric_constants
        help: use the associated constant instead
             |
        1493 |             Token::U64(u64::MAX),
             |                             ~~~
    
        warning: usage of a legacy numeric method
            --> test_suite/tests/test_de_error.rs:1510:29
             |
        1510 |             Token::U64(u64::max_value()),
             |                             ^^^^^^^^^^^
             |
             = help: for further information visit https://rust-lang.github.io/rust-clippy/master/index.html#legacy_numeric_constants
        help: use the associated constant instead
             |
        1510 |             Token::U64(u64::MAX),
             |                             ~~~

--- test_suite/tests/test_de_error.rs
+++ test_suite/tests/test_de_error.rs
@@ -1459,7 +1459,7 @@ fn test_duration_overflow_seq() {
     assert_de_tokens_error::<Duration>(
         &[
             Token::Seq { len: Some(2) },
-            Token::U64(u64::max_value()),
+            Token::U64(u64::MAX),
             Token::U32(1_000_000_000),
             Token::SeqEnd,
         ],
@@ -1476,7 +1476,7 @@ fn test_duration_overflow_struct() {
                 len: 2,
             },
             Token::Str("secs"),
-            Token::U64(u64::max_value()),
+            Token::U64(u64::MAX),
             Token::Str("nanos"),
             Token::U32(1_000_000_000),
             Token::StructEnd,
@@ -1490,7 +1490,7 @@ fn test_systemtime_overflow_seq() {
     assert_de_tokens_error::<SystemTime>(
         &[
             Token::Seq { len: Some(2) },
-            Token::U64(u64::max_value()),
+            Token::U64(u64::MAX),
             Token::U32(1_000_000_000),
             Token::SeqEnd,
         ],
@@ -1507,7 +1507,7 @@ fn test_systemtime_overflow_struct() {
                 len: 2,
             },
             Token::Str("secs_since_epoch"),
-            Token::U64(u64::max_value()),
+            Token::U64(u64::MAX),
             Token::Str("nanos_since_epoch"),
             Token::U32(1_000_000_000),
             Token::StructEnd,
@@ -1522,7 +1522,7 @@ fn test_systemtime_overflow() {
     assert_de_tokens_error::<SystemTime>(
         &[
             Token::Seq { len: Some(2) },
-            Token::U64(u64::max_value()),
+            Token::U64(u64::MAX),
             Token::U32(0),
             Token::SeqEnd,
         ],
