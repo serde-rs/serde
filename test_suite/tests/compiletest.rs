7aafa263146f9140b18879c114a69fe96b2ebffc
David Tolnay <dtolnay@gmail.com>
2024-06-01 22:13:43 -0700

    Fill in ignore reasons in all #[ignore] attributes

--- test_suite/tests/compiletest.rs
+++ test_suite/tests/compiletest.rs
@@ -1,6 +1,6 @@
-#[cfg_attr(target_os = "emscripten", ignore)]
-#[rustversion::attr(not(nightly), ignore)]
-#[cfg_attr(miri, ignore)]
+#[cfg_attr(target_os = "emscripten", ignore = "disabled on Emscripten")]
+#[rustversion::attr(not(nightly), ignore = "requires nightly")]
+#[cfg_attr(miri, ignore = "incompatible with miri")]
 #[allow(unused_attributes)]
 #[test]
 fn ui() {
