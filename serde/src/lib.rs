9e6158e9e6be4a9e599643a12539f2e9d9ab4506
David Tolnay <dtolnay@gmail.com>
2024-06-10 09:45:45 -0700

    Nightly no longer requires error_in_core feature
        warning: the feature `error_in_core` has been stable since 1.81.0-nightly and no longer requires an attribute to enable
           --> serde/src/lib.rs:108:43
            |
        108 | #![cfg_attr(feature = "unstable", feature(error_in_core, never_type))]
            |                                           ^^^^^^^^^^^^^
            |
            = note: `#[warn(stable_features)]` on by default

--- serde/src/lib.rs
+++ serde/src/lib.rs
@@ -105,7 +105,7 @@
 // discussion of these features please refer to this issue:
 //
 //    https://github.com/serde-rs/serde/issues/812
-#![cfg_attr(feature = "unstable", feature(error_in_core, never_type))]
+#![cfg_attr(feature = "unstable", feature(never_type))]
 #![allow(unknown_lints, bare_trait_objects, deprecated)]
 // Ignored clippy and clippy_pedantic lints
 #![allow(
