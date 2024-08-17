a22d492fa9f98df9431571acd4317a5565dbd7e0
Mingun <alexander_sergey@mail.ru>
2024-08-17 10:18:24 +0500

    Cover all cases in ContentDeserializer::deserialize_option

--- serde/src/private/de.rs
+++ serde/src/private/de.rs
@@ -1290,10 +1290,19 @@ mod content {
         where
             V: Visitor<'de>,
         {
+            // Covered by tests/test_annotations.rs
+            //      flatten::with_optional_field::*
             match self.content {
                 Content::None => visitor.visit_none(),
                 Content::Some(v) => visitor.visit_some(ContentDeserializer::new(*v)),
                 Content::Unit => visitor.visit_unit(),
+                // This case is to support data formats which do not encode an
+                // indication whether a value is optional. An example of such a
+                // format is JSON, and a counterexample is RON. When requesting
+                // `deserialize_any` in JSON, the data format never performs
+                // `Visitor::visit_some` but we still must be able to
+                // deserialize the resulting Content into data structures with
+                // optional fields.
                 _ => visitor.visit_some(self),
             }
         }
