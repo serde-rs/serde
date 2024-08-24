bd8d9d72ba727f34c14262cb22e258ba25710d4d
Mingun <alexander_sergey@mail.ru>
2024-08-24 19:02:04 +0500

    cargo fmt

--- serde/src/private/de.rs
+++ serde/src/private/de.rs
@@ -1658,11 +1658,8 @@ mod content {
         where
             V: de::DeserializeSeed<'de>,
         {
-            let visitor = VariantDeserializer {
-                de: self.value,
-            };
-            seed.deserialize(self.variant)
-                .map(|v| (v, visitor))
+            let visitor = VariantDeserializer { de: self.value };
+            seed.deserialize(self.variant).map(|v| (v, visitor))
         }
     }
 
@@ -2308,11 +2305,8 @@ mod content {
         where
             V: de::DeserializeSeed<'de>,
         {
-            let visitor = VariantRefDeserializer {
-                de: self.value,
-            };
-            seed.deserialize(self.variant)
-                .map(|v| (v, visitor))
+            let visitor = VariantRefDeserializer { de: self.value };
+            seed.deserialize(self.variant).map(|v| (v, visitor))
         }
     }
 
