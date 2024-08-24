e7a320b4ac236f066abe321a0cc05ef4c69ccdb9
Mingun <alexander_sergey@mail.ru>
2024-08-24 20:26:27 +0500

    Ensure that sequence is empty in InternallyTaggedUnitVisitor::visit_seq
    When intermediate buffer is used, we can just ignore data, because it already was read
    from the original deserializer to the buffer and check for the emptiness was performed
    in another place. Now we reading directly from the original deserializer and should
    ensure empty sequence by self.
    
    Fixed (1):
        unit_variant_with_unknown_fields

--- serde_core/src/de/value.rs
+++ serde_core/src/de/value.rs
@@ -1105,7 +1105,9 @@ where
     }
 }
 
-struct ExpectedInSeq(usize);
+/// Number of elements still expected in a sequence. Does not include already
+/// read elements.
+struct ExpectedInSeq(pub usize);
 
 impl Expected for ExpectedInSeq {
     fn fmt(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
@@ -1581,6 +1583,8 @@ where
     }
 }
 
+/// Number of elements still expected in a map. Does not include already read
+/// elements.
 struct ExpectedInMap(usize);
 
 impl Expected for ExpectedInMap {
