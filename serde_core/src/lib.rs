be255d62c81a88226fddfd1b1b97a76a6b5054fb
David Tolnay <dtolnay@gmail.com>
2025-09-13 14:45:43 -0700

    Release 1.0.220

--- serde_core/src/lib.rs
+++ serde_core/src/lib.rs
@@ -17,7 +17,7 @@
 ////////////////////////////////////////////////////////////////////////////////
 
 // Serde types in rustdoc of other crates get linked to here.
-#![doc(html_root_url = "https://docs.rs/serde/1.0.219")]
+#![doc(html_root_url = "https://docs.rs/serde/1.0.220")]
 // Support using Serde without the standard library!
 #![cfg_attr(not(feature = "std"), no_std)]
 // Show which crate feature enables conditionally compiled APIs in documentation.
