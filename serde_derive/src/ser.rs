1cc8999e4d1bac7fb72e9df9eb794492d68a4cce
dishmaker <141624503+dishmaker@users.noreply.github.com>
2026-02-16 14:43:11 +0100

    serde_derive: use sum_tokens in all `let len =` calculations

--- serde_derive/src/ser.rs
+++ serde_derive/src/ser.rs
@@ -294,6 +294,7 @@ fn serialize_tuple_struct(
     }
 }
 
+/// `quote!(#init + #item + ...)`
 fn sum_tokens(init: TokenStream, iter: impl Iterator<Item = TokenStream>) -> TokenStream {
     let mut tokens = init;
     for item in iter {
@@ -355,18 +356,16 @@ fn serialize_struct_as_struct(
 
     let let_mut = mut_if(serialized_fields.peek().is_some() || tag_field_exists);
 
-    let len = serialized_fields
-        .map(|field| match field.attrs.skip_serializing_if() {
+    let len = sum_tokens(
+        quote!(#tag_field_exists as usize),
+        serialized_fields.map(|field| match field.attrs.skip_serializing_if() {
             None => quote!(1),
             Some(path) => {
                 let field_expr = get_member(params, field, &field.member);
                 quote!(if #path(#field_expr) { 0 } else { 1 })
             }
-        })
-        .fold(
-            quote!(#tag_field_exists as usize),
-            |sum, expr| quote!(#sum + #expr),
-        );
+        }),
+    );
 
     quote_block! {
         let #let_mut __serde_state = _serde::Serializer::serialize_struct(__serializer, #type_name, #len)?;
@@ -840,15 +839,16 @@ fn serialize_tuple_variant(
 
     let let_mut = mut_if(serialized_fields.peek().is_some());
 
-    let len = serialized_fields
-        .map(|(i, field)| match field.attrs.skip_serializing_if() {
+    let len = sum_tokens(
+        quote!(0),
+        serialized_fields.map(|(i, field)| match field.attrs.skip_serializing_if() {
             None => quote!(1),
             Some(path) => {
                 let field_expr = field_i(i);
                 quote!(if #path(#field_expr) { 0 } else { 1 })
             }
-        })
-        .fold(quote!(0), |sum, expr| quote!(#sum + #expr));
+        }),
+    );
 
     match context {
         TupleVariant::ExternallyTagged {
@@ -917,16 +917,17 @@ fn serialize_struct_variant(
 
     let let_mut = mut_if(serialized_fields.peek().is_some());
 
-    let len = serialized_fields
-        .map(|field| {
+    let len = sum_tokens(
+        quote!(0),
+        serialized_fields.map(|field| {
             let member = &field.member;
 
             match field.attrs.skip_serializing_if() {
                 Some(path) => quote!(if #path(#member) { 0 } else { 1 }),
                 None => quote!(1),
             }
-        })
-        .fold(quote!(0), |sum, expr| quote!(#sum + #expr));
+        }),
+    );
 
     match context {
         StructVariant::ExternallyTagged {
