f32168bb77788891165194ca0c5318bd0b1de9d0
Tiago Dinis <tiagodinis33@proton.me>
2026-06-27 21:52:28 +0100

    Update content.rs

--- serde_core/src/private/content.rs
+++ serde_core/src/private/content.rs
@@ -14,7 +14,10 @@ pub enum Content<'de> {
     U16(u16),
     U32(u32),
     U64(u64),
-
+    #[cfg(feature = "floats")]
+    F32(f32),
+    #[cfg(feature = "floats")]
+    F64(f64),
     I8(i8),
     I16(i16),
     I32(i32),
